92d366fb87be6856bda6d297c20f4c004bee151c
FarhanJulystio <farhanjt70@gmail.com>
2021-06-09 23:26:58 +0700

    API Volley

---
+++ API_Volley/app/src/main/java/com/farhan/api_volley/Util/ServerAPI.java
@@ -0,0 +1,8 @@
+package com.farhan.api_volley.Util;
+
+public class ServerAPI {
+    public static final String URL_DATA = "http://10.212.139.234/crud/Mahasiswa/Api";
+    public static final String URL_INSERT = "http://10.212.139.234/crud/Mahasiswa/ApiInsert";
+    public static final String URL_DELETE = "http://10.212.139.234/crud/Mahasiswa/ApiDelete";
+    public static final String URL_UPDATE = "http://10.212.139.234/crud/Mahasiswa/ApiUpdate";
+}
